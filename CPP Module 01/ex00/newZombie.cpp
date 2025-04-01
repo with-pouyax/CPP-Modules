0678fd907191d33464ddbfd1d533a118477a1c5c
with-pouyax <75066687+with-pouyax@users.noreply.github.com>
2025-04-01 14:11:35 +0200

    add a condition for when zomie has no name

--- CPP Module 01/ex00/newZombie.cpp
+++ CPP Module 01/ex00/newZombie.cpp
@@ -4,6 +4,10 @@
 // the benefit of using a pointer here is that we can return the address of the Zombie object that we create in the function
 Zombie* newZombie(std::string name) {
     try {
+        // If name is empty, provide a default name
+        if (name.empty()) {
+            name = "Unnamed Zombie";
+        }
         return new Zombie(name);
     } catch (const std::bad_alloc& error) { //we refer to the error which is an object of type std::bad_alloc
         std::cerr << "Memory allocation failed: " << error.what() << std::endl; //what() is a member function of the std::bad_alloc class that returns a string describing the error
