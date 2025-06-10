3ee994b399e0bc999ff1090735b5abdd02362bdf
“with-pouyax” <75066687+with-pouyax@users.noreply.github.com>
2025-06-10 14:47:14 +0200

    M06 E02, finish for now again

--- CPP Module 06/ex02/main.cpp
+++ CPP Module 06/ex02/main.cpp
@@ -11,6 +11,7 @@ int main() {
             std::cerr << "Failed to generate object in pointer test" << std::endl;
             continue; // this continue means that we skip the rest of the loop and go to the next iteration
                       // if we had a break, we would exit the loop and go to the next code block
+                      //so in last iteration, if we reach this continue, we will not delete the pointer and we just break the loop
         }
         std::cout << "Generated type: ";
         identify(ptr); // we identify the type of the object
@@ -28,7 +29,7 @@ int main() {
         }
         std::cout << "Generated type: ";
         try {
-            identify(*ptr);
+            identify(*ptr); //ptr is a pointer to a Base object, so we dereference it to get the object
         } catch (const std::exception& e) {
             std::cerr << "Error during reference identification: " << e.what() << std::endl;
         }
