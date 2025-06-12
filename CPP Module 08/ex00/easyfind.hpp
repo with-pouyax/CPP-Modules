e3dd920362d813283138076558459a3a1ee4a3c8
“with-pouyax” <75066687+with-pouyax@users.noreply.github.com>
2025-06-12 18:59:03 +0000

    M08 e00 add some comments

--- CPP Module 08/ex00/easyfind.hpp
+++ CPP Module 08/ex00/easyfind.hpp
@@ -5,7 +5,10 @@
 #include <stdexcept>
 
 template<typename T>
-typename T::iterator easyfind(T& container, int value) {
+typename T::iterator easyfind(T& container, int value) {     // we have a function called easyfind that returns an iterator (a pointer to an element in the container) to a T type container
+    
+    // find receives the range we are looking for as 1st and 2nd parameters, and the value we are looking for as the 3rd parameter
+    // if the value is not found, it returns an iterator to the end of the container
     typename T::iterator it = std::find(container.begin(), container.end(), value);
     if (it == container.end())
         throw std::runtime_error("Value not found in container");
