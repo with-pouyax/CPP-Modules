44a522921dfce3fa596864c64b0cadb339c1e3cf
with-pouyax <75066687+with-pouyax@users.noreply.github.com>
2025-07-01 15:08:52 +0200

    M09 e01 first scratch 2

--- CPP Module 09/ex01/main.cpp
+++ CPP Module 09/ex01/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]) {
         int result = calculator.evaluate(expression);
         std::cout << result << std::endl;
     } catch (const std::exception& e) {
-        std::cerr << "Error" << std::endl;
+        std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
     
